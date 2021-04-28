32c5e919759fac01fb650d342ba26d89dfa42851
alextula26 <alex_marc86@mail.ru>
2021-04-28 11:44:33 +0300

    added base config webpack.config.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,21 @@
 // @ts-check
+import _ from 'lodash';
 
-import init from './init.js';
+class Example {
+  constructor(element) {
+    this.element = element;
+  }
 
-init();
+  init(date) {
+    this.element.textContent = `Hello, World! Now date is ${date}`;
+    console.log('ehu!');
+  }
+}
+
+const init = (date) => {
+  const element = document.getElementById('point');
+  const obj = new Example(element);
+  obj.init(date);
+};
+
+init(_.now());
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-    ],
+  entry: './src/js/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './template.html',
-    }),
-  ],
 };
